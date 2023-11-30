fb3d2663e1d5d868d7681e54869d1c9906b833d6
Andrew Segales <andysejas444@gmail.com>
2023-11-30 12:00:15 -0400

    Header

---
+++ src/componets/Cabeza.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cabeza() {
+  return (
+    <div className='cajon'>
+        <p className='Titu'>Exercícios</p> <p className='Num'>4</p>
+    </div>
+  )
+}
+
+export default Cabeza
